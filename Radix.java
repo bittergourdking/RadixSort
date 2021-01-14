37b2d141495e237e34237db165730c01ca3fd7fd
bittergourdking <>
2021-01-14 09:02:26 -0500

    testing new method

--- Radix.java
+++ Radix.java
@@ -3,19 +3,68 @@ public class Radix {
     if (col < 0) {
       throw new IllegalArgumentException("column cannot be negative!");
     }
-    return Math.abs((n / (int)Math.pow(10, col)) % 10);
+    return (int)((n / Math.pow(10, col)) % 10);
   }
 
   public static int length(int n) {
+    if (n == 0) {
+      return 1;
+    }
     return (n + "").length();
   }
 
   public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < buckets.length; i++) {
       original.extend(buckets[i]);
     }
   }
 
+  public static void radixSortSimple(SortableLinkedList data) {
+  SortableLinkedList[] buckets = new SortableLinkedList[10];
+  for (int i = 0; i < buckets.length; i++) {
+    buckets[i] = new SortableLinkedList();
+  }
+  int digitNum = 0;
+  boolean yes = true;
+  while (yes) {
+    yes = false;
+    while (data.size() > 0) {
+      System.out.println(data.get(0));
+      int currentNum = data.remove(0);
+      int digitEnd = nth(currentNum, digitNum);
+      buckets[digitEnd].add(currentNum);
+      if (Radix.length(currentNum) > digitNum + 1) {
+        yes = true;
+      }
+    }
+    digitNum++;
+    merge(data, buckets);
+  }
+}
+
+public static void radixSort(SortableLinkedList data) {
+  SortableLinkedList[] buckets = new SortableLinkedList[19];
+  for (int i = 0; i <= 18; i++) {
+    buckets[i] = new SortableLinkedList();
+  }
+  int digitNum = 0;
+  boolean yes = true;
+  while (yes) {
+    yes = false;
+    while (data.size() > 0) {
+      int currentNum = data.remove(0);
+      int digitEnd = nth(currentNum, digitNum)+9;
+      buckets[digitEnd].add(currentNum);
+      if (Radix.length(currentNum) > digitNum + 1) {
+        yes = true;
+      }
+    }
+    digitNum++;
+    merge(data, buckets);
+  }
+}
+
+/*
   public static void radixSortSimple(SortableLinkedList data) {
     int maxLength = 0;
     int current = 0;
@@ -64,4 +113,5 @@ public class Radix {
     data.extend(negative);
     data.extend(positive);
   }
+  */
 }
