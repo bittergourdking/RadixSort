8d6872036c65de95cae412d1008426e33ff50800
bittergourdking <>
2021-01-14 09:13:32 -0500

    all should be good!

--- Radix.java
+++ Radix.java
@@ -20,98 +20,46 @@ public class Radix {
   }
 
   public static void radixSortSimple(SortableLinkedList data) {
-  SortableLinkedList[] buckets = new SortableLinkedList[10];
-  for (int i = 0; i < buckets.length; i++) {
-    buckets[i] = new SortableLinkedList();
-  }
-  int colNum = 0;
-  boolean next = true;
-  while (next) {
-    next = false;
-    while (data.size() > 0) {
-      System.out.println(data.get(0));
-      int currentNum = data.remove(0);
-      int digitEnd = nth(currentNum, colNum);
-      buckets[digitEnd].add(currentNum);
-      if (length(currentNum) > colNum + 1) {
-        next = true;
-      }
-    }
-    colNum++;
-    merge(data, buckets);
-  }
-}
-
-public static void radixSort(SortableLinkedList data) {
-  SortableLinkedList[] buckets = new SortableLinkedList[19];
-  for (int i = 0; i <= 18; i++) {
-    buckets[i] = new SortableLinkedList();
-  }
-  int colNum = 0;
-  boolean next = true;
-  while (next) {
-    next = false;
-    while (data.size() > 0) {
-      int currentNum = data.remove(0);
-      int digitEnd = nth(currentNum, colNum)+9;
-      buckets[digitEnd].add(currentNum);
-      if (length(currentNum) > colNum + 1) {
-        next = true;
-      }
-    }
-    colNum++;
-    merge(data, buckets);
-  }
-}
-
-/*
-  public static void radixSortSimple(SortableLinkedList data) {
-    int maxLength = 0;
-    int current = 0;
-    int start = 0;
     SortableLinkedList[] buckets = new SortableLinkedList[10];
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < buckets.length; i++) {
       buckets[i] = new SortableLinkedList();
     }
-    for (int j = 0; j < data.size(); j++) {
-      current = length(data.get(j));
-      if (current > maxLength) {
-        maxLength = current;
-      }
-      current = data.remove(0);
-      buckets[nth(current, 0)].add(current);
-    }
-    merge(data, buckets);
-    for (int k = 1; k < maxLength; k++) {
-      for (int l = start; l < data.size(); l++) {
-        current = data.get(l);
-        if (length(current) >= k) {
-          buckets[nth(current, k)].add(current);
-          data.remove(l);
+    int colNum = 0;
+    boolean next = true;
+    while (next) {
+      next = false;
+      while (data.size() > 0) {
+        int currentNum = data.remove(0);
+        int digitEnd = nth(currentNum, colNum);
+        buckets[digitEnd].add(currentNum);
+        if (length(currentNum) > colNum + 1) {
+          next = true;
         }
       }
-      start = data.size() - 1;
+      colNum++;
       merge(data, buckets);
     }
   }
 
   public static void radixSort(SortableLinkedList data) {
-    SortableLinkedList negative = new SortableLinkedList();
-    SortableLinkedList positive = new SortableLinkedList();
-    for (int i = 0; i < data.size(); i++) {
-      if (data.get(i) < 0) {
-        negative.add(data.remove(i));
-      } else {
-        positive.add(data.remove(i));
+    SortableLinkedList[] buckets = new SortableLinkedList[19];
+    for (int i = 0; i <= 18; i++) {
+      buckets[i] = new SortableLinkedList();
+    }
+    int colNum = 0;
+    boolean next = true;
+    while (next) {
+      next = false;
+      while (data.size() > 0) {
+        int currentNum = data.remove(0);
+        int digitEnd = nth(currentNum, colNum)+9;
+        buckets[digitEnd].add(currentNum);
+        if (length(currentNum) > colNum + 1) {
+          next = true;
+        }
       }
+      colNum++;
+      merge(data, buckets);
     }
-    radixSortSimple(positive);
-    radixSortSimple(negative);
-    for (int j = negative.size() - 1; j > -1; j--) {
-      negative.add(negative.remove(j));
-    }
-    data.extend(negative);
-    data.extend(positive);
   }
-  */
 }
